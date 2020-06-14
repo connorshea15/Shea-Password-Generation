4a88cb540d9ddb8c9be5b52359d4a85b67a96442
connorshea15 <connorshea15@gmail.com>
2020-06-14 13:28:07 -0700

    Setup the basic password concatenation

--- Develop/script.js
+++ Develop/script.js
@@ -8,9 +8,20 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   //var password = generatePassword();
   //var passwordText = document.querySelector("#password");
-  
+  var password = "";
   // Ask the user how long the password should be
   var passwordLength = window.prompt("How long would you like your password to be? (Between 8 and 128)");
+  // make sure the input is good here 
+   /* while (typeof passwordLength < 8 || passwordLength > 128) {
+      window.alert("Please put a numberic value between 8 and 128");
+    } */
+    passwordLength = parseInt(passwordLength);
+    for (i = 0; i < passwordLength; i++) {
+      console.log(randomNumber());
+      password += randomNumber();
+    }
+    window.alert("Your password is " + password);
+  
   // Ask the user if they want lowercase characters included
   var lowerCaseConfirm = window.confirm("Would you like to include lower case characters in your password?");
   // Ask the user if they want uppercase characters included
@@ -26,9 +37,20 @@ function writePassword() {
   console.log(numericConfirm);
   console.log(specialCharacterConfirm);
 
-  passwordText.value = password;
+  //passwordText.value = password;
 
 }
 
+var checkInput = function() {
+
+
+};
+
+// function to create a random number between 0 and 9
+var randomNumber = function () {
+  return Math.floor(Math.random() * 10);
+};
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
